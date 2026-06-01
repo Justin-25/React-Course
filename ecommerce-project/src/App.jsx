5bc869eb73f3d0efd8886576c10cbccdd7da1f41
JustinDev <justinmandia25@gmail.com>
2026-06-02 02:03:30 +0800

    Generate HTML for the cart items

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,18 +1,28 @@
 import { Routes, Route } from "react-router";
-
-import "./App.css";
-
 import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/Checkout/CheckoutPage";
 import { OrdersPage } from "./pages/Orders/OrdersPage";
 import { TrackingPage } from "./pages/Tracking/TrackingPage";
 import { NotFound } from "./pages/NotFound";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/cart-items?expand=product')
+      .then((response) => {
+        setCart(response.data);
+      });
+  }, []);
+
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/" element={<HomePage cart={cart} />} />
+      <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="/orders" element={<OrdersPage />} />
       <Route path="/tracking" element={<TrackingPage />} />
 
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,23 +1,20 @@
 import axios from "axios";
 import { Link } from "react-router";
 import { Header } from "../components/Header";
-import "./HomePage.css";
 import { useEffect, useState } from "react";
+import { formatMoney } from "../utils/money";
+import "./HomePage.css";
 
-export function HomePage() {
+export function HomePage({
+  cart
+}) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products")
+    axios.get('/api/products')
       .then((response) => {
         setProducts(response.data);
       });
-
-    axios.get("http://localhost:3000/api/cart-items")
-      .then((response) => {
-        setCart(response.data)
-      })
   }, []);
 
   return (
@@ -53,7 +50,7 @@ export function HomePage() {
                 </div>
 
                 <div className="product-price">
-                  ${(Math.round(product.priceCents) / 100).toFixed(2)}
+                  {formatMoney(product.priceCents)}
                 </div>
 
                 <div className="product-quantity-container">
